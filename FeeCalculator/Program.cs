d4bf681eab8cfeefcb667e744c1f40b1174993fe
ReisEdgar <edgar.reis447@gmail.com>
2020-07-05 13:34:24 +0300

    initial commit

---
+++ FeeCalculator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FeeCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
