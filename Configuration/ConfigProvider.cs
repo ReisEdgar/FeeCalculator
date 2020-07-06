a05e93a56cce74af6e85d50bc20b58006cf0a019
ReisEdgar <edgar.reis447@gmail.com>
2020-07-06 16:01:38 +0300

    add DI and Configuration providers

---
+++ Configuration/ConfigProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Configuration
+{
+    public class ConfigProvider
+    {
+        public static string FeeSaveFilePath { get; } = "C:\\Users\\Ivan\\source\\repos\\FeeCalculator\\fees.txt";
+        public static string TransactionsFilePath { get; } = "C:\\Users\\Ivan\\source\\repos\\FeeCalculator\\transactions.txt";
+        public static string TeliaGenericDiscount { get; } = "0,1";
+        public static string CircleKGenericDiscount { get; } = "0,2";
+        public static string FixedInvoiceFee { get; } = "29";
+        public static string TransactionFeePercentage { get; } = "0,01";
+    }
+}
---
+++ Infrastructure/DependecyInjectionService.cs
@@ -0,0 +1,56 @@
+﻿using Logic.Mappers;
+using Logic;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using FileRepository;
+using Microsoft.Extensions.Configuration;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Infrastructure
+{
+    public static class DependencyInjectionService
+    {
+        public static void RegisterDependencies()
+        {
+            Register<ITransactionRepository, TransactionRepository>();
+            Register<ITransactionFeeService, TransactionFeeService>();
+            Register<ITransactionDiscountService, TransactionDiscountService>();
+            Register<ITransactionMapper, TransactionMapper>();
+
+        }
+        static readonly IDictionary<Type, Type> types = new Dictionary<Type, Type>();
+
+        public static void Register<TContract, TImplementation>()
+        {
+            types[typeof(TContract)] = typeof(TImplementation);
+        }
+
+        public static T Resolve<T>()
+        {
+            return (T)Resolve(typeof(T));
+        }
+
+        public static object Resolve(Type contract)
+        {
+            Type implementation = types[contract];
+            ConstructorInfo constructor = implementation.GetConstructors()[0];
+            ParameterInfo[] constructorParameters = constructor.GetParameters();
+            if (constructorParameters.Length == 0)
+            {
+                return Activator.CreateInstance(implementation);
+            }
+
+            List<object> parameters = new List<object>(constructorParameters.Length);
+            foreach (ParameterInfo parameterInfo in constructorParameters)
+            {
+                parameters.Add(Resolve(parameterInfo.ParameterType));
+            }
+
+            return constructor.Invoke(parameters.ToArray());
+        }
+    }
+
+}
+
